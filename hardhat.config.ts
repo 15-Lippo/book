04da8f53b866ff807b2d105d980b8aca0fafa39d
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-11-29 01:26:47 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,18 @@ require("dotenv").config({ path: __dirname + "/.env" });
 
 module.exports = {
   networks: {
-    // ganache: {
-    //   url: "HTTP://127.0.0.1:7545",
+    // ethereum: {
+    //   url: "https://rpc.flashbots.net",
+    //   accounts: {
+    //     mnemonic:
+    //       "tag palace accident hidden delay escape involve fetch mushroom corn settle doctor",
+    //     path: "m/44'/60'/0'/0",
+    //     initialIndex: 0,
+    //     count: 10,
+    //   },
+    networks: {
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
     //   accounts: {
     //     mnemonic:
     //       "tag palace accident hidden delay escape involve fetch mushroom corn settle doctor",
@@ -13,15 +23,15 @@ module.exports = {
     //     count: 10,
     //   },
 
-    //   chainId: 1337,
+    //   chainId: 137,
     // },
-    mumbai: {
-      url: `https://rpc-mumbai.maticvigil.com`,
+    mainnet: {
+      url: `https://polygon-rpc.com`,
       gasPrice: 36000000000,
       accounts: [`0x${process.env.POLYGON_PRIVATE_KEY}`],
     },
-    harmonytest: {
-      url: `https://api.s0.b.hmny.io`,
+    harmonymainnet: {
+      url: 'https://api.harmony.one',
       gas: 56000000000,
       accounts: [`0x${process.env.HARMONY_PRIVATE_KEY}`],
     },
